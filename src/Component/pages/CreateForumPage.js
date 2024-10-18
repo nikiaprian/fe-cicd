3f772f70bcf412ed4072ecb76c58f4de591d78cc
nikiaprian <aprianniki69@gmail.com>
2024-10-18 15:05:18 +0700

    v0.4.18 perubahan endpoint backend

--- src/Component/pages/CreateForumPage.js
+++ src/Component/pages/CreateForumPage.js
@@ -26,7 +26,7 @@ function CreateForumPage() {
     inputTag.forEach((tag) => data.append("tags", tag?.text));
 
     await axios
-      .post("http://docker-alb-be-67897166.us-west-2.elb.amazonaws.com:9090/forums/new", data, {
+      .post("http://internal-docker-alb-be-463725261.us-west-2.elb.amazonaws.com:9090/forums/new", data, {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${token}`,
