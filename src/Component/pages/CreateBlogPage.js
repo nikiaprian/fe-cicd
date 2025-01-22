37cd941a0b640ef80cbbc2f56a512d0dfef31821
nikiaprian <aprianniki69@gmail.com>
2025-01-22 09:12:49 +0700

    v0.4.20 perubahan endpoint backend

--- src/Component/pages/CreateBlogPage.js
+++ src/Component/pages/CreateBlogPage.js
@@ -31,7 +31,7 @@ function CreateBlogPage() {
     inputTag.forEach((tag) => data.append("tags", tag?.text));
 
     await axios
-      .post("http://docker-alb-be-897750170.us-west-2.elb.amazonaws.com:9090/blogs/new", data, {
+      .post("http://docker-alb-be-1664573111.us-west-2.elb.amazonaws.com:9090/blogs/new", data, {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${token}`,
--- src/Component/pages/DetailBlogPage.js
+++ src/Component/pages/DetailBlogPage.js
@@ -19,7 +19,7 @@ function DetailBlogPage() {
   const fetchSetComment = useBlogStore((state) => state.fetchSetComment);
   const fetchCommentInput = useBlogStore((state) => state.fetchCommentInput);
   useEffect(() => {
-    fetchBlogId(`http://docker-alb-be-897750170.us-west-2.elb.amazonaws.com:9090/blogs/${idblog}`, key);
+    fetchBlogId(`http://docker-alb-be-1664573111.us-west-2.elb.amazonaws.com:9090/blogs/${idblog}`, key);
     fetchSetComment(idblog);
     //eslint-disable-next-line
   }, [idblog, fetchBlogId, key]);
