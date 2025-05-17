6a40bf402ae5cffc6f59fcea1439eea2e647ce65
nikiaprian <aprianniki69@gmail.com>
2025-05-17 15:42:17 +0700

    niki2

--- src/Component/molecules/DetailForum/CheckedCard.js
+++ src/Component/molecules/DetailForum/CheckedCard.js
@@ -17,7 +17,7 @@ function CheckedCard(props) {
     console.log(checked);
     await axios
       .patch(
-        `http://docker-alb-be-1291943132.us-west-2.elb.amazonaws.com:9090/forum/comment/${props?.id}/selected-answer`,
+        `http://docker-alb-be-783427459.us-west-2.elb.amazonaws.com:9090/forum/comment/${props?.id}/selected-answer`,
         { is_answer: checked },
         {
           headers: {
--- src/Component/pages/DetailBlogPage.js
+++ src/Component/pages/DetailBlogPage.js
@@ -19,7 +19,7 @@ function DetailBlogPage() {
   const fetchSetComment = useBlogStore((state) => state.fetchSetComment);
   const fetchCommentInput = useBlogStore((state) => state.fetchCommentInput);
   useEffect(() => {
-    fetchBlogId(`http://docker-alb-be-1291943132.us-west-2.elb.amazonaws.com:9090/blogs/${idblog}`, key);
+    fetchBlogId(`http://docker-alb-be-783427459.us-west-2.elb.amazonaws.com:9090/blogs/${idblog}`, key);
     fetchSetComment(idblog);
     //eslint-disable-next-line
   }, [idblog, fetchBlogId, key]);
